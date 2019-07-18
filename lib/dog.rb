10f9fd83ec6613cb93ce9ab10ab8bc2fa08d98aa
eliothowes1090 <ejhowes@gmail.com>
2019-07-18 09:23:36 +0100

    Defined Dog Class

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,40 @@
 # Add your code here
+require 'pry'
+
+class Dog
+
+    attr_reader :name
+
+    @@all = []
+    @@count = 0
+
+    def initialize(name)
+        @name = name
+        @@count += 1
+
+        @@all << self
+    end
+
+    def name
+        @name
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.all
+        @@all
+        # binding.pry
+    end
+
+    def self.print_all
+        # binding.pry
+        @@all.each {|dog| puts dog.name}
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+
+end
